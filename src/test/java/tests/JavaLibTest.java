f648a3cb9e097176b0df8f84f32a7926ecce2c10
jcstritt <jcstritt@hispeed.ch>
2016-03-11 13:10:07 +0100

    Change in JavaLibTest.java

--- src/test/java/tests/JavaLibTest.java
+++ src/test/java/tests/JavaLibTest.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.*;
  * @author jcstritt
  */
 public class JavaLibTest {
+  private static String CLASS_TO_CHECK = "target/test-classes/beans/Compte.class";
 
   public JavaLibTest() {
   }
@@ -35,8 +36,7 @@ public class JavaLibTest {
   @Test
   public void testGetJavaClassVersion() {
     StackTracer.printCurrentTestMethod();
-    String filename = "target/classes/ch/jcsinfo/system/JavaLib.class";
-    int[] result = JavaLib.getJavaClassVersion(filename);
+    int[] result = JavaLib.getJavaClassVersion(CLASS_TO_CHECK);
     System.out.println("- Java major: " + result[0]);
     System.out.println("- Java minor: " + result[1]);
     assertTrue(result[0] > 0);
@@ -48,8 +48,7 @@ public class JavaLibTest {
   @Test
   public void testGetJavaClassPlatform() {
     StackTracer.printCurrentTestMethod();
-    String filename = "target/classes/ch/jcsinfo/system/JavaLib.class";
-    String result = JavaLib.getJavaClassPlatform(filename);
+    String result = JavaLib.getJavaClassPlatform(CLASS_TO_CHECK);
     System.out.println("- Java platform: " + result);
     assertTrue(result.contains("JDK") || result.contains("J2SE"));
   }
