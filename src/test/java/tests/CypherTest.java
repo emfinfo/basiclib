075c29a2b1e8dcd2fb381d8d4aefc2a3b1987771
jcstritt <strittjc@edufr.ch>
2017-11-29 19:35:32 +0100

    Test update

--- src/test/java/tests/CypherTest.java
+++ src/test/java/tests/CypherTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import ch.jcsinfo.system.StackTracer;
+import ch.jcsinfo.util.ConvertLib;
 import ch.jcsinfo.util.Cypher;
 import java.nio.charset.Charset;
 import org.junit.AfterClass;
@@ -12,44 +13,60 @@ import org.junit.runners.MethodSorters;
 
 /**
  * Test des méthodes principales de la classe correspondante.
- * 
+ *
  * @author jcstritt
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CypherTest {
-  private static String toEncode = "06h30, Bonjour les amis de la pêche en Gruyère !";
-  
-  private static String key = "PabloPicasso";
-  private static String encoded = "";
-  
+//  private static String toEncode = "06h30, Bonjour les amis de la pêche en Gruyère !";
+//  private static String key = "PabloPicasso";
+  private static String original[] = {"Demo AG", "Demo Ltd", "Demo SA"};
+  private static String encoded[] = {"", "", ""};
+  private static String key = "Marco" + "Polo";
+
   @BeforeClass
   public static void setUpClass() throws Exception {
     System.out.println("\n>>> " + StackTracer.getCurrentClass() + " <<<");
-//    System.out.println("Default charset: " + Charset.defaultCharset().displayName());
+    System.out.println("Default charset: " + Charset.defaultCharset().displayName());
   }
 
   @AfterClass
   public static void tearDownClass() throws Exception {
     System.out.println();
-  }  
+  }
 
   @Test
   public void test01_encrypt() {
     StackTracer.printCurrentTestMethod();
-    encoded = Cypher.encrypt(toEncode, key, Charset.defaultCharset());
-    System.out.println("  - to encrypt : " + toEncode);
-    System.out.println("  - crypted    : " + encoded);
-    assertTrue(!encoded.equals(toEncode));
+    int maxLen = 0;
+    for (String one : original) {
+      if (one.length() > maxLen) {
+        maxLen = one.length();
+      }
+    }
+
+    for (int i = 0; i < original.length; i++) {
+      encoded[i] = Cypher.encrypt(original[i], key, Charset.defaultCharset());
+      System.out.println(ConvertLib.fillString(maxLen, ' ', original[i]) + "\t" + encoded[i]);
+      assertTrue(!encoded.equals(original[i]));
+    }
   }
-  
+
   @Test
   public void test02_decrypt() {
     StackTracer.printCurrentTestMethod();
-    String toDecode = encoded;
-    String decoded = Cypher.decrypt(toDecode, key, Charset.defaultCharset());
-    System.out.println("  - to decrypt : " + toDecode);
-    System.out.println("  - decrypted  : " + decoded);
-    assertTrue(decoded.equals(toEncode));
-  }  
+    int maxLen = 0;
+    for (String one : encoded) {
+      if (one.length() > maxLen) {
+        maxLen = one.length();
+      }
+    }
+
+    for (int i = 0; i < encoded.length; i++) {
+      String decoded = Cypher.decrypt(encoded[i], key, Charset.defaultCharset());
+      System.out.println(ConvertLib.fillString(maxLen, ' ', encoded[i]) + "\t" + decoded);
+      assertTrue(decoded.equals(original[i]));
+    }
+  }
 
 }
