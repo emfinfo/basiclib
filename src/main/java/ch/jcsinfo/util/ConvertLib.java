e6ced3178f3cc00f8e5a55bdc3978a4d752507cc
jcstritt <strittjc@edufr.ch>
2017-12-29 01:55:39 +0100

    In convertLib, methods rename for some hash functions

--- src/main/java/ch/jcsinfo/util/ConvertLib.java
+++ src/main/java/ch/jcsinfo/util/ConvertLib.java
@@ -686,16 +686,22 @@ public class ConvertLib {
   }
 
   /**
-   * Rehâche une clé de 128 caractères avec le double sel contenu dans la clé.
-   * Cette clé est composée comme suit :<br>
+   * Recalcule une nouvelle clé de 128 caractères basée sur une clé existante fournie,
+   * également de 128 caractères. La partie "motDePasse" (48 à 112) est rehachée (même
+   * s'il s'agit déjà d'une empreinte -hash-) avec une composition des deux "sels" déjà
+   * contenus dans la clé. Cette méthode sert donc essentiellement côté serveur pour
+   * s'affranchir du problème de l'homme du milieu.<br>
+   *
+   * Une clé est composée comme suit :<br>
    * sel1 + motDePasse + sel2<br>
    * 48c  + 64c        + 16c = 128c<br>
    *
-   * @param key la clé contenant le mot de passe déjà pré-hâché et un double sel
-   * @param params éventuellement la clé de la BD pour y récupérer les sels
-   * @return la clé rehâchée pouvant être considérée comme le mot de passe final
+   * @param key une clé existante contenant un mot de passe déjà hâché depuis le client et un double sel
+   * @param params éventuellement la clé dans la BD pour y récupérer les sels et reclaculer une clé à comparer
+   *
+   * @return la clé recalculée
    */
-  public static String rehashKeyWithSalt(String key, Object... params) {
+  public static String computeServerKey(String key, Object... params) {
     String s1 = key.substring(0, 48);
     String s2 = key.substring(112);
     if (params.length == 1) {
@@ -704,27 +710,32 @@ public class ConvertLib {
       s2 = dbKey.substring(112);
     }
     String salt = s1 + s2;
-    String pwd = key.substring(48, 112);
-    String result = s1 + hashWithSHA256(pwd + salt) + s2;
-//    System.out.println("key:    " + key);
-//    System.out.println("pwd:    " + pwd);
-//    System.out.println("salt:   " + salt);
-//    System.out.println("result: " + result);
-    return result;
+    String hash = key.substring(48, 112);
+    String newKey = s1 + hashWithSHA256(hash + salt) + s2;
+    System.out.println("key:     " + key);
+    System.out.println("salt:   " + salt);
+    System.out.println("hash:   " + hash);
+    System.out.println("rehash: " + newKey);
+    return newKey;
   }
 
   /**
-   * Hache une nouvelle combinaison (nom de login + mot de passe).
+   * Calcule une clé de 128 caractères dont la partie "motDePasse" est haché en SHA-256.
+   * Deux sels sont également calculés et rajoutés à la clé finale.<br>
+   * <br>
+   * Une clé est donc composée comme suit :<br>
+   * sel1 + motDePasse + sel2<br>
+   * 48c  + 64c        + 16c = 128c<br>
    *
    * @param loginName un String avec un nom de login (username)
    * @param pwd un String avec un mot de passe
    * @return le mot de passe haché sur 128 car.
    */
-  public static String hashNewKey(String loginName, String pwd) {
+  public static String computeClientKey(String loginName, String pwd) {
     String s1 = getHexString(MathLib.randomStr('A', 'z', 24));
     String s2 = getHexString(MathLib.randomStr('A', 'z', 8));
     String key = s1 + hashWithSHA256(loginName + hashWithSHA256(pwd)) + s2;
-    return rehashKeyWithSalt(key);
+    return computeServerKey(key);
   }
 
 
